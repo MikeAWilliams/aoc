1e5aa7e253590285bc5e607536bd2cb6d78cc145
Mike Williams <mumakin@gmail.com>
2022-12-04 21:07:01 -0700

    formatting

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -7,65 +7,74 @@ fn main() {
     println!("Problem 2 {}", solve_problem_two(&input));
 }
 
-fn elf_pair_is_redundant(first:&WorkIndices, second:&WorkIndices)->bool{
-    (first.start >= second.start && first.stop <= second.stop) ||
-    (second.start >= first.start && second.stop <= first.stop)
+fn elf_pair_is_redundant(first: &WorkIndices, second: &WorkIndices) -> bool {
+    (first.start >= second.start && first.stop <= second.stop)
+        || (second.start >= first.start && second.stop <= first.stop)
 }
 
-fn elf_pair_overlaps(first:&WorkIndices, second:&WorkIndices)->bool{
-    elf_pair_is_redundant(first, second)||(
-        (first.stop >= second.start && first.stop <= second.stop) ||
-        (second.stop >= first.start && second.stop <= first.stop)
-    )
+fn elf_pair_overlaps(first: &WorkIndices, second: &WorkIndices) -> bool {
+    elf_pair_is_redundant(first, second)
+        || ((first.stop >= second.start && first.stop <= second.stop)
+            || (second.stop >= first.start && second.stop <= first.stop))
 }
 
-struct WorkIndices{
-    pub start:i32,
-    pub stop:i32,
+struct WorkIndices {
+    pub start: i32,
+    pub stop: i32,
 }
 
-fn score_elf_par(work_indicies:&Vec<WorkIndices>, predicate: fn(&WorkIndices,&WorkIndices)->bool)->i32{
-        match predicate(&work_indicies[0], &work_indicies[1]){
-            true => 1,
-            _ => 0,
-        }
+fn score_elf_par(
+    work_indicies: &Vec<WorkIndices>,
+    predicate: fn(&WorkIndices, &WorkIndices) -> bool,
+) -> i32 {
+    match predicate(&work_indicies[0], &work_indicies[1]) {
+        true => 1,
+        _ => 0,
+    }
 }
 
 fn solve_problem_one(input: &Vec<Vec<WorkIndices>>) -> i32 {
-    input.iter().map(|elf_pair|{
-        score_elf_par(elf_pair, elf_pair_is_redundant)
-   }).sum()
+    input
+        .iter()
+        .map(|elf_pair| score_elf_par(elf_pair, elf_pair_is_redundant))
+        .sum()
 }
 
 fn solve_problem_two(input: &Vec<Vec<WorkIndices>>) -> i32 {
-    input.iter().map(|elf_pair|{
-        score_elf_par(elf_pair, elf_pair_overlaps)
-    }).sum()
+    input
+        .iter()
+        .map(|elf_pair| score_elf_par(elf_pair, elf_pair_overlaps))
+        .sum()
 }
 
-fn get_work_indices_from_str(input: &str)->WorkIndices{
-    input.split_once('-')
-        .map(|(start_str, end_str)|{
-            WorkIndices { start: start_str.parse::<i32>().unwrap(), stop: end_str.parse::<i32>().unwrap() }
-        }).unwrap()
+fn get_work_indices_from_str(input: &str) -> WorkIndices {
+    input
+        .split_once('-')
+        .map(|(start_str, end_str)| WorkIndices {
+            start: start_str.parse::<i32>().unwrap(),
+            stop: end_str.parse::<i32>().unwrap(),
+        })
+        .unwrap()
 }
-
 
 fn get_lines_from_file(file: &str) -> Vec<String> {
     let file_contents = fs::read_to_string(file).unwrap();
-    file_contents.split(|c| c=='\n')
-    .map(|s| s.to_string())
-    .collect()
+    file_contents
+        .split(|c| c == '\n')
+        .map(|s| s.to_string())
+        .collect()
 }
 
 fn get_input_from_file(file: &str) -> Vec<Vec<WorkIndices>> {
     let lines = get_lines_from_file(file);
-    lines.iter()
+    lines
+        .iter()
         .map(|line| {
             line.split(|c| c == ',')
-            .map(|index_pair_str| get_work_indices_from_str(index_pair_str))
-            .collect()}).collect()
-
+                .map(|index_pair_str| get_work_indices_from_str(index_pair_str))
+                .collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
