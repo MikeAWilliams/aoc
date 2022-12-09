ba1f5ffcd0d3e528d3ab0dc1ffcba31adf2a0407
Mike Williams <mwilliams@alteryx.com>
2022-12-09 11:30:06 -0700

    start to process

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -7,12 +7,45 @@ fn main() {
     println!("Problem 1 {}", solve_problem_one(&input));
 }
 
+fn generate_directory_graph(console: &Vec<String>) -> DirectoryGraph {
+    let mut result = DirectoryGraph {
+        nodes: vec![],
+        parents: vec![],
+        dependencies: vec![],
+    };
+    // start with a / dir
+    result.nodes.push(DirectoryNode {
+        filesSize: 0,
+        totalSize: 0,
+        name: "root".to_string(),
+    });
+    let mut current_node_index = 0;
+    let mut input_line: usize = 0;
+    while input_line < console.len() {
+        input_line += 1;
+    }
+    result
+}
+
 fn solve_problem_one(input: &Vec<String>) -> i32 {
+    let mut graph = generate_directory_graph(input);
     0
 }
 
 fn solve_problem_two(input: &Vec<String>) -> i32 {
     0
+}
+
+struct DirectoryNode {
+    pub filesSize: i32,
+    pub totalSize: i32,
+    pub name: String,
+}
+
+struct DirectoryGraph {
+    pub nodes: Vec<DirectoryNode>,
+    pub parents: Vec<usize>,
+    pub dependencies: Vec<Vec<usize>>,
 }
 
 fn get_lines_from_file(file: &str) -> Vec<String> {
