ba46f738e19eaf4ba477f134850b5378b50cb8d4
Mike Williams <mwilliams@alteryx.com>
2022-12-22 14:43:00 -0700

    built a graph

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,10 +1,57 @@
 use std::fs;
+use substring::Substring;
 
 fn main() {
     let input = get_input_from_file("example_input.txt");
     //let input = get_input_from_file("problem_input.txt");
 
     println!("Problem 1 {}", solve_problem_one(&input));
+}
+
+fn add_sub_dir(dir: &str, current_index: i32, graph: &mut DirectoryGraph) {
+    let newIndex = graph.add_node_if_new(dir.to_string());
+    graph.add_dependency_if_new(current_index as usize, newIndex);
+}
+
+fn process_command_line_default(
+    line: &String,
+    current_index: i32,
+    graph: &mut DirectoryGraph,
+) -> i32 {
+    //println!("default {}", line);
+    if line.starts_with("dir ") {
+        let destination = line.substring(4, line.len());
+        //println!("its a dir {}", destination);
+        add_sub_dir(destination, current_index, graph);
+    } else {
+        let file_size = line.split_whitespace().next().unwrap();
+        graph.nodes[current_index as usize].filesSize += file_size.parse::<i32>().unwrap();
+    }
+    current_index
+}
+
+fn process_command_line_cd(newDir: &str, current_index: i32, graph: &DirectoryGraph) -> i32 {
+    println!("its a cd {}", newDir);
+    if newDir == ".." {
+        graph.parents[current_index as usize] as i32
+    } else {
+        graph
+            .nodes
+            .iter()
+            .position(|node| node.name == newDir)
+            .unwrap() as i32
+    }
+}
+
+fn process_command_line(line: &String, current_index: i32, graph: &mut DirectoryGraph) -> i32 {
+    return if line.starts_with("$ cd ") {
+        let parts = line.split(' ').collect::<Vec<&str>>();
+        process_command_line_cd(parts[2], current_index, graph)
+    } else if line != "$ ls" {
+        process_command_line_default(line, current_index, graph)
+    } else {
+        current_index
+    };
 }
 
 fn generate_directory_graph(console: &Vec<String>) -> DirectoryGraph {
@@ -14,14 +61,12 @@ fn generate_directory_graph(console: &Vec<String>) -> DirectoryGraph {
         dependencies: vec![],
     };
     // start with a / dir
-    result.nodes.push(DirectoryNode {
-        filesSize: 0,
-        totalSize: 0,
-        name: "root".to_string(),
-    });
+    result.add_node_if_new("root".to_string());
     let mut current_node_index = 0;
-    let mut input_line: usize = 0;
+    let mut input_line: usize = 1;
     while input_line < console.len() {
+        current_node_index =
+            process_command_line(&console[input_line], current_node_index, &mut result);
         input_line += 1;
     }
     result
@@ -29,6 +74,7 @@ fn generate_directory_graph(console: &Vec<String>) -> DirectoryGraph {
 
 fn solve_problem_one(input: &Vec<String>) -> i32 {
     let mut graph = generate_directory_graph(input);
+    graph.print();
     0
 }
 
@@ -46,6 +92,62 @@ struct DirectoryGraph {
     pub nodes: Vec<DirectoryNode>,
     pub parents: Vec<usize>,
     pub dependencies: Vec<Vec<usize>>,
+}
+
+impl DirectoryGraph {
+    fn add_node_if_new(&mut self, name: String) -> usize {
+        let possition = self.nodes.iter().position(|node| node.name == name);
+        match possition {
+            Some(index) => index,
+            None => {
+                self.nodes.push(DirectoryNode {
+                    filesSize: 0,
+                    totalSize: 0,
+                    name: name,
+                });
+                self.dependencies.push(vec![]);
+                self.parents.push(0);
+                self.nodes.len() - 1
+            }
+        }
+    }
+
+    fn add_dependency_if_new(&mut self, parent: usize, child: usize) {
+        if let None = self.dependencies[parent]
+            .iter()
+            .position(|item| item == &child)
+        {
+            self.dependencies[parent].push(child);
+            self.parents[child] = parent;
+        }
+    }
+
+    fn print(&self) {
+        println!("Nodes");
+        for node in &self.nodes {
+            println!("{}, {}, {}", node.name, node.filesSize, node.totalSize)
+        }
+
+        println!("Parents");
+        for node_index in 0..self.parents.len() {
+            let node_name = &self.nodes[node_index].name;
+            let parent_name = &self.nodes[self.parents[node_index]].name;
+            println!("node {} has parent {}", node_name, parent_name);
+        }
+
+        println!("Children");
+        for node_index in 0..self.dependencies.len() {
+            let node_name = &self.nodes[node_index].name;
+            println!(
+                "node {} has children {}",
+                node_name,
+                self.dependencies[node_index]
+                    .iter()
+                    .map(|item| item.to_string() + ",")
+                    .collect::<String>()
+            );
+        }
+    }
 }
 
 fn get_lines_from_file(file: &str) -> Vec<String> {
