ea76d87047aade14658509bbed0495c2cfd67a71
Mike Williams <mumakin@gmail.com>
2023-12-07 11:30:03 -0700

    day 3 done

--- 2023/day03/day3.cpp
+++ 2023/day03/day3.cpp
@@ -5,14 +5,42 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <numeric>
 #include <ranges>
 #include <string>
+
+const std::vector<std::pair<int, int>> ADJACENT_OFFSETS{
+    {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1},
+};
+
+struct PartNumber {
+    int  rowIndex, columnIndex;
+    int  value, lenghth;
+    bool Intersects(int row, int column) const;
+};
 
 std::tuple<bool, int, int> IdentifyNumber(const char* in);
 bool                       SymbolAdjacent(
                           const std::vector<std::string>& search, int row, int column, int width);
+std::vector<PartNumber>  FindPartNumbers(const std::vector<std::string>& lines);
 int                      Solve(const std::vector<std::string>& lines);
+int                      SolvePart2(const std::vector<std::string>& lines);
 std::vector<std::string> GetPuzzleInput();
+
+TEST_CASE("PartNumber Intersects", "[day3]") {
+    PartNumber testObject{10, 10, 100, 4};
+    // not in the right row
+    REQUIRE_FALSE(testObject.Intersects(9, 10));
+    REQUIRE_FALSE(testObject.Intersects(11, 10));
+    // not in the right column
+    REQUIRE_FALSE(testObject.Intersects(10, 9));
+    REQUIRE_FALSE(testObject.Intersects(10, 14));
+
+    REQUIRE(testObject.Intersects(10, 10));
+    REQUIRE(testObject.Intersects(10, 11));
+    REQUIRE(testObject.Intersects(10, 12));
+    REQUIRE(testObject.Intersects(10, 13));
+}
 
 TEST_CASE("symbolAdjacent", "[day3]") {
     // no symbol
@@ -149,6 +177,28 @@ TEST_CASE("part 1 from file", "[day3]") {
     REQUIRE(514969 == Solve(GetPuzzleInput()));
 }
 
+TEST_CASE("part 2 simple", "[day3]") {
+    REQUIRE(
+        467835 == SolvePart2(std::vector<std::string>{
+                      // clang-format off
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598.."
+    }));
+    // clang-format on
+}
+
+TEST_CASE("part 2 from file", "[day3]") {
+    REQUIRE(78915902 == SolvePart2(GetPuzzleInput()));
+}
+
 bool SymbolAdjacent(
     const std::vector<std::string>& search, int row, int column, int width) {
     if (row >= search.size() || column >= search[row].length()) {
@@ -156,12 +206,9 @@ bool SymbolAdjacent(
     }
     // pairs of {column offset, row offset}
     // all thse count as adjacent
-    const std::vector<std::pair<int, int>> adjacency{
-        {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1},
-    };
 
     for (int i = column; i < column + width; ++i) {
-        for (const auto& offset : adjacency) {
+        for (const auto& offset : ADJACENT_OFFSETS) {
             auto check_row = row + offset.second;
             if (check_row >= search.size()) {
                 continue;
@@ -194,7 +241,49 @@ std::tuple<bool, int, int> IdentifyNumber(const char* in) {
 }
 
 int Solve(const std::vector<std::string>& lines) {
-    int result = 0;
+    auto partNumbers{FindPartNumbers(lines)};
+    return std::accumulate(
+        std::cbegin(partNumbers), std::cend(partNumbers), 0,
+        [](auto runningTotal, auto part) { return runningTotal + part.value; });
+}
+
+int SolvePart2(const std::vector<std::string>& lines) {
+    auto result = 0;
+    auto partNumbers{FindPartNumbers(lines)};
+    for (int rowIndex = 0; rowIndex < static_cast<int>(lines.size());
+         ++rowIndex) {
+        for (int columnIndex = 0; columnIndex < lines[rowIndex].size();
+             ++columnIndex) {
+            if ('*' != lines[rowIndex][columnIndex]) {
+                continue;
+            }
+            auto adjacentParts = partNumbers | std::views::filter([rowIndex, columnIndex](const auto& part){
+                return !(ADJACENT_OFFSETS | std::views::filter([rowIndex, columnIndex, &part](const auto& offset){
+                    return part.Intersects(rowIndex + offset.first, columnIndex + offset.second);
+                })).empty();
+            });
+            // i'm pretty sure i'm using rangees wrong here becasue I want adjacentParts.size() but its not there. Also no support for const filter result or cbegin cend
+            if(2 == std::distance(std::begin(adjacentParts), std::end(adjacentParts)))
+            {
+                // fold_left is in c++23 but not until gcc13 and i'm on 12 
+                // not sure if this is correct syntax because I can't compile it.
+                //int gearRatio = std::ranges::fold_left(adjacentParts, 1, [](int valueSoFar, const auto& partNumber){
+                   //return valueSoFar * partNumber.value; 
+                //});
+                // lets use a silly for loop. who needs ranges anyway
+                int gearRatio = 1;
+                for(const auto& partNumber : adjacentParts){
+                    gearRatio *= partNumber.value;
+                }
+                result += gearRatio;
+            }
+        }
+    }
+    return result;
+}
+
+std::vector<PartNumber> FindPartNumbers(const std::vector<std::string>& lines) {
+    std::vector<PartNumber> result;
 
     for (int rowIndex = 0; rowIndex < static_cast<int>(lines.size());
          ++rowIndex) {
@@ -210,7 +299,7 @@ int Solve(const std::vector<std::string>& lines) {
             if (isNumber) {
                 // std::cout << value << " " << length << std::endl;
                 if (SymbolAdjacent(lines, rowIndex, columnIndex, length)) {
-                    result += value;
+                    result.push_back({rowIndex, columnIndex, value, length});
                 }
                 columnIndex += length;
             }
@@ -232,4 +321,12 @@ std::vector<std::string> GetPuzzleInput() {
     }
     file.close();
     return result;
+}
+
+bool PartNumber::Intersects(int row, int column) const {
+    if (this->rowIndex != row) {
+        return false;
+    }
+    return column >= this->columnIndex &&
+           column < this->columnIndex + this->lenghth;
 }
