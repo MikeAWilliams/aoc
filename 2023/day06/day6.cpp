a16cf49f365fa840e9289b934aa77008719abf2a
Mike Williams <mumakin@gmail.com>
2024-01-02 16:18:15 -0700

    FindPossibleRaces test complete

--- 2023/day06/day6.cpp
+++ 2023/day06/day6.cpp
@@ -1,5 +1,6 @@
 // main.cpp
 #include <catch2/catch_test_macros.hpp>
+#include <compare>
 #include <vector>
 
 struct RaceRecord {
@@ -8,8 +9,9 @@ struct RaceRecord {
 };
 
 struct RaceResult {
-    int buttonTime;
-    int raceDistance;
+    int  buttonTime;
+    int  raceDistance;
+    auto operator<=>(const RaceResult&) const = default;
 };
 
 std::vector<RaceResult> FindPossibleRaces(const RaceRecord& record);
@@ -17,8 +19,20 @@ std::vector<RaceResult> FindPossibleRaces(const RaceRecord& record);
 TEST_CASE("FindPossibleRaces", "[day6]") {
     auto result = FindPossibleRaces({7, 9});
     REQUIRE(8 == result.size());
+    REQUIRE(result[0] == RaceResult{0, 0});
+    REQUIRE(result[1] == RaceResult{1, 6});
+    REQUIRE(result[2] == RaceResult{2, 10});
+    REQUIRE(result[3] == RaceResult{3, 12});
+    REQUIRE(result[4] == RaceResult{4, 12});
+    REQUIRE(result[5] == RaceResult{5, 10});
+    REQUIRE(result[6] == RaceResult{6, 6});
+    REQUIRE(result[7] == RaceResult{7, 0});
 }
 
 std::vector<RaceResult> FindPossibleRaces(const RaceRecord& record) {
-    return std::vector<RaceResult>{};
+    std::vector<RaceResult> result;
+    for (int holdTime = 0; holdTime <= record.time; ++holdTime) {
+        result.emplace_back(holdTime, (record.time - holdTime) * holdTime);
+    }
+    return result;
 }
