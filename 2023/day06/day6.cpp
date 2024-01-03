2e6145ca0775e5aebb3020eb16738fa14d0f866c
Mike Williams <mumakin@gmail.com>
2024-01-02 19:50:56 -0700

    day6 done

--- 2023/day06/day6.cpp
+++ 2023/day06/day6.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <compare>
 #include <cstdlib>
+#include <iostream>
 #include <limits>
 #include <numeric>
 #include <ranges>
@@ -10,8 +11,8 @@
 #include <vector>
 
 struct RaceRecord {
-    int time;
-    int distance;
+    int64_t time;
+    int64_t distance;
 };
 
 struct RaceResult {
@@ -23,7 +24,8 @@ struct RaceResult {
 std::vector<RaceResult> FindPossibleRaces(const RaceRecord& record);
 int                     CountWinningRaces(
                         const RaceRecord& record, const std::vector<RaceResult>& results);
-int SolvePart1(std::vector<RaceRecord>& puzzleInput);
+int     SolvePart1(std::vector<RaceRecord>& puzzleInput);
+int64_t SolvePart2(const RaceRecord& input);
 
 TEST_CASE("FindPossibleRaces", "[day6]") {
     auto result = FindPossibleRaces({7, 9});
@@ -37,6 +39,7 @@ TEST_CASE("FindPossibleRaces", "[day6]") {
     REQUIRE(result[6] == RaceResult{6, 6});
     REQUIRE(result[7] == RaceResult{7, 0});
 }
+
 TEST_CASE("CountWinningRaces", "[day6]") {
     RaceRecord record{7, 9};
     auto       races = FindPossibleRaces(record);
@@ -44,12 +47,18 @@ TEST_CASE("CountWinningRaces", "[day6]") {
 
     REQUIRE(4 == wins);
 }
+
 TEST_CASE("Solve Part 1", "[day6]") {
     std::vector<RaceRecord> exampleInput{{7, 9}, {15, 40}, {30, 200}};
     REQUIRE(288 == SolvePart1(exampleInput));
     std::vector<RaceRecord> puzzleInput{
         {40, 233}, {82, 1011}, {84, 1110}, {92, 1487}};
     REQUIRE(3316275 == SolvePart1(puzzleInput));
+}
+
+TEST_CASE("Solve Part 2", "[day6]") {
+    REQUIRE(71503 == SolvePart2({71530, 940200}));
+    REQUIRE(27'102'791 == SolvePart2({40828492, 233'101'111'101'487}));
 }
 
 std::vector<RaceResult> FindPossibleRaces(const RaceRecord& record) {
@@ -78,12 +87,32 @@ int CountWinningRaces(
     }
     return result;
 }
+
 int SolvePart1(std::vector<RaceRecord>& puzzleInput) {
     // inelegent. Doesn't use algorithms or ranges. but I just want an answer
     // right now
     int result = 1;
     for (const auto& input : puzzleInput) {
         result *= CountWinningRaces(input, FindPossibleRaces(input));
+    }
+    return result;
+}
+
+int64_t SolvePart2(const RaceRecord& input) {
+    int64_t result = 0;
+    // thanks to Sigmund for suggesting brute force
+    double possibleSolutions = input.time;
+    for (int possibleSpeed = 0; possibleSpeed < input.time; ++possibleSpeed) {
+        auto distance = (input.time - possibleSpeed) * possibleSpeed;
+        if (distance > input.distance) {
+            ++result;
+        }
+        auto fraction = possibleSpeed / possibleSolutions;
+        int  rounder  = fraction * 100000;
+        if (rounder == 10000 || rounder == 200000 || rounder == 50000 ||
+            rounder == 80000 || rounder == 90000) {
+            std::cout << fraction << "\n";
+        }
     }
     return result;
 }
